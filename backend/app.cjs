62c37f23733ad17cf802593cf7b691062a4db6a7
Miklós Ónodi <onodim@kolcsey-szajol.hu>
2026-01-14 20:26:47 +0100

    Started writing serious logic.

--- backend/app.cjs
+++ backend/app.cjs
@@ -1,11 +1,15 @@
 const express = require('express')
 const path = require('path')
-const data = require('./data.cjs')
+const {people} = require('./data.cjs')
 const app = express()
 
+function logger(req, res, next){
+  console.log(req.method, req.url, req.queries)
+  next()
+}
 
 
-// app.use(express.static(path.join(__dirname, './public')))
+app.use([express.static(path.join(__dirname, './public')), logger])
 
 // app.get('/', (req, res) => {
 //   res.status(200).sendFile(path.resolve(__dirname, './frontend/html/index.html'))
@@ -14,6 +18,17 @@ const app = express()
 //   res.json(data)
 // })
 
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, './frontend/html/index.html'))
+})
+
+
+app.get('/data', (req, res) => {
+  res.json(people)
+})
+app.post('/data', (req, res) => {
+  
+})
 
 
 
