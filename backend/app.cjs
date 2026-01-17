869252d3f27ad39ea59e9b340b51ec9a36924bd6
Ónodi Miklós <mikonodi33@gmail.com>
2026-01-17 12:11:48 +0100

    Fixed small bugs and typeos.

--- backend/app.cjs
+++ backend/app.cjs
@@ -14,21 +14,22 @@ app.get('/', (req, res) => {
 
 
 app.get('/data', (req, res) => {
-  res.json({sucess: true, data: people})
+  res.json({success: true, data: people})
 })
 app.get('/data/param/:personId', (req, res) => {
   const {personId} = req.params
   const person = people.find(
-    (people) => people.id === Number(personId)
+    (person) => person.id === Number(personId)
   )
   if(!person){
-    return res.status(404).json({ sucess: false, msg: "person not found" })
+    return res.status(404).json({ success: false, msg: "person not found" })
   }
-  return res.json({sucess: true, data: person})
+  return res.json({success: true, data: person})
 })
 app.get('/data/query', (req, res) => {
   const {search, limit} = req.query
   let sortedPeople = [...people]
+  search.trim().toLowerCase()
   if(search) {
     sortedPeople = sortedPeople.filter((person) => {
       return person.name.startsWith(search)
@@ -38,15 +39,15 @@ app.get('/data/query', (req, res) => {
     sortedPeople = sortedPeople.slice(0, Number(limit))
   }
   if (sortedPeople.length < 1) {
-    return res.status(200).json({ sucess: true, data: [] })
+    return res.status(200).json({ success: true, data: [] })
   }
-  res.status(200).json({sucess: true, data: sortedPeople})
+  res.status(200).json({success: true, data: sortedPeople})
 })
 
 app.post('/data', (req, res) => {
   const { name } = req.body
   if (!name) {
-    return res.status(400).json({sucess: false, msg: 'please provide name'})
+    return res.status(400).json({success: false, msg: 'please provide name'})
   }
 
   const ids = people.map((e) => {return e.id})
@@ -60,7 +61,7 @@ app.post('/data', (req, res) => {
 
   people.push(newPerson)
   res.status(201).json({
-    sucess: true,
+    success: true,
     data: people
   })
 })
